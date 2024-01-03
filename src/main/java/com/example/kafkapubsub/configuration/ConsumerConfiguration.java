90c5434b5a2d82c5d70a0cedd9a50ab6c4035797
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-01-03 15:25:45 +0700

    update 03/01

--- src/main/java/com/example/kafkapubsub/configuration/ConsumerConfiguration.java
+++ src/main/java/com/example/kafkapubsub/configuration/ConsumerConfiguration.java
@@ -1,11 +1,8 @@
 package com.example.kafkapubsub.configuration;
 
-import com.example.kafkapubsub.model.MessageDto;
 import com.example.kafkapubsub.model.User;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +10,8 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
@@ -40,7 +39,7 @@ public class ConsumerConfiguration {
 //        return converter;
 //    }
     @Bean
-    public ConsumerFactory<String,User> userConsumerFactory(){
+    public ConsumerFactory<String, User> userConsumerFactory(){
         Map<String,Object> props = createDefaultProps();
         props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS,StringDeserializer.class);
         props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS,JsonDeserializer.class);
@@ -51,6 +50,7 @@ public class ConsumerConfiguration {
     public ConcurrentKafkaListenerContainerFactory<String, User> userKafkaListenerFactory() {
         ConcurrentKafkaListenerContainerFactory<String,User> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(userConsumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         //factory.setRecordMessageConverter(multiTypeConverter());
         return factory;
     }
@@ -59,7 +59,7 @@ public class ConsumerConfiguration {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerAddress);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,ErrorHandlingDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES,"*");
 
--- src/main/java/com/example/kafkapubsub/producer/ConsumerService.java
+++ src/main/java/com/example/kafkapubsub/producer/ConsumerService.java
@@ -3,69 +3,118 @@ package com.example.kafkapubsub.producer;
 import com.example.kafkapubsub.model.User;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.PartitionOffset;
-import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.listener.AbstractConsumerSeekAware;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 @Service
 @RequiredArgsConstructor
-public class ConsumerService {
+    public class ConsumerService extends AbstractConsumerSeekAware {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
+
+    private final Map<Thread, ConsumerSeekCallback> callbackForThread = new ConcurrentHashMap<>();
+
+    private final Map<TopicPartition, ConsumerSeekCallback> callbacks = new ConcurrentHashMap<>();
+
+    private final Map<ConsumerSeekCallback, List<TopicPartition>> callbacksToTopic = new ConcurrentHashMap<>();
+
+    private void waitUntil(long t) {
+        try {
+            Thread.sleep(t);
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        System.out.println("Assignments:" + assignments);
+        super.onPartitionsAssigned(assignments, callback);
+        assignments.keySet().forEach(partition -> {
+            callback.seek(partition.topic(),partition.partition(),15);
+        });
+//        callback.seek("userTopic", 0,12);
+    }
 
     @KafkaListener(topics = "userTopic",
             groupId = "group1",
             containerFactory = "userKafkaListenerFactory")
     public void listener1(ConsumerRecord<String, User> record,
-                            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+                          @Header(KafkaHeaders.RECEIVED_PARTITION) int partition,
+                          @Header(KafkaHeaders.OFFSET)int offset,
+                          Acknowledgment acknowledgment) {
+        //callbackForThread.get(Thread.currentThread()).seek("uesrTopic",0,20);
         User user = record.value();
-        System.out.println("Listener 1-Group1: Consume user with fullname "+user.getFullname()+" from partition: " + partition);
+        logger.info("Listener 1-Group1: "+user.getFullname()+" from partition: " + partition + " with offset: "+offset);
+        //throwException();
+        acknowledgment.acknowledge();
     }
+
 
     @KafkaListener(topics = "adminTopic",
             groupId = "group1",
             containerFactory = "userKafkaListenerFactory")
     public void listener5(ConsumerRecord<String, User> record,
-                          @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+                          @Header(KafkaHeaders.RECEIVED_PARTITION)int partition,
+                          @Header(KafkaHeaders.OFFSET)int offset,
+                          Acknowledgment acknowledgment) {
         User user = record.value();
-        System.out.println("Listener 5-Group2: Received "+user.getFullname()+" from partition: " + partition);
+        logger.info("Listener 5-Group2: "+user.getFullname()+" from partition: " + partition + " with offset: "+offset);
+        //throwException();
+        acknowledgment.acknowledge();
     }
 
-    @KafkaListener(topics = "adminTopic",
-            groupId = "group2",
-            containerFactory = "userKafkaListenerFactory")
-
-    public void listener2(ConsumerRecord<String, User> record,
-                              @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
-        User user = record.value();
-        System.out.println("Listener 2-Group1: Received: "+user.getFullname()+ " from partition: " + partition);
+    private void throwException(){
+        throw new RuntimeException("Error");
     }
 
-    @KafkaListener(
-            topicPartitions = @TopicPartition(topic = "userTopic",
-                    partitionOffsets = {
-                            @PartitionOffset(partition = "0", initialOffset = "0")}),
-            containerFactory = "userKafkaListenerFactory")
-    public void listener3(
-            @Payload User user,
-            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
-        System.out.println(
-                "Listener 3-Group1: Received: " + user.getFullname()
-                        + " from partition: " + partition);
-    }
+//    @KafkaListener(topics = "adminTopic",
+//            groupId = "group2",
+//            containerFactory = "userKafkaListenerFactory")
+//    public void listener2(ConsumerRecord<String, User> record,
+//                              @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+//        User user = record.value();
+//        logger.info("Listener 2-Group1: "+user.getFullname()+ " from partition: " + partition);
+//    }
 
-    @KafkaListener(
-            topicPartitions = @TopicPartition(topic = "userTopic",
-                    partitionOffsets = {
-                            @PartitionOffset(partition = "0", initialOffset = "10")}),
-            containerFactory = "userKafkaListenerFactory")
-    public void listener4(
-            @Payload User user,
-            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
-        System.out.println(
-                "Listener 4-Group1: Received: " + user.getFullname()
-                        + " from partition: " + partition);
-    }
+//    @KafkaListener(
+//            topicPartitions = @TopicPartition(topic = "userTopic",
+//                    partitionOffsets = {
+//                            @PartitionOffset(partition = "0", initialOffset = "0")}),
+//            containerFactory = "userKafkaListenerFactory")
+//    public void listener3(
+//            @Payload User user,
+//            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+//        System.out.println(
+//                "Listener 3-Group1: Received: " + user.getFullname()
+//                        + " from partition: " + partition);
+//    }
+
+//    @KafkaListener(
+//            topicPartitions = @TopicPartition(topic = "userTopic",
+//                    partitionOffsets = {
+//                            @PartitionOffset(partition = "0", initialOffset = "10")}),
+//            containerFactory = "userKafkaListenerFactory")
+//    public void listener4(
+//            @Payload User user,
+//            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition) {
+//        System.out.println(
+//                "Listener 4-Group1: Received: " + user.getFullname()
+//                        + " from partition: " + partition);
+//    }
 }
